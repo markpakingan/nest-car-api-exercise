ebd4fe947c7e86c34d74780f6fc96083ddf5b9f2
markpakingan <116990353+markpakingan@users.noreply.github.com>
2024-02-25 10:24:19 -0800

    update type any to class interface for DTO

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,8 +6,8 @@ import { UsersService } from './users.service';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
-
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
     constructor(private usersService: UsersService){}
 
@@ -16,7 +16,6 @@ export class UsersController {
         this.usersService.create(body.email, body.password)
     }
 
-    @Serialize(UserDto)
     @Get('/:id')
     async findUser(@Param('id') id: string){
 
