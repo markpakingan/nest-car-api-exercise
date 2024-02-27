2bd2151a9a539be6cf991a20751fdf6915384602
markpakingan <116990353+markpakingan@users.noreply.github.com>
2024-02-27 08:13:29 -0800

    added salt on hash

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,6 +1,10 @@
  import { BadRequestException, Injectable } from "@nestjs/common";
  import { UsersService } from "./users.service";
+ import { randomBytes, scrypt as _scrypt } from "crypto";
+ import { promisify } from "util";
 
+
+ const scrypt = promisify(_scrypt);
 
  @Injectable()
  export class AuthService {
@@ -9,19 +13,26 @@
 
     async signup(email: string, password: string) {
 
-        //see if email is in us.
+        //see if email is in use
 
         const users = await this.usersService.find(email);
         if(users){
             throw new BadRequestException('email in use')
         }
 
-        //hash the users password
+        //generate a salt
+        const salt = randomBytes(8).toString('hex');
 
+        //hash the users password
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+
+        //Join the hashed result and the salt together
+        const userPassword = salt + "." + hash.toString('hex')
 
         //Create a new user and save it
 
-
+    
         //Return the user
     }
 
