c8034f068404d534aa06afe2d8275f56e1913e32
markpakingan <116990353+markpakingan@users.noreply.github.com>
2024-02-27 21:36:00 -0800

    added the signin feature

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,8 @@
- import { BadRequestException, Injectable } from "@nestjs/common";
+ import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
  import { UsersService } from "./users.service";
  import { randomBytes, scrypt as _scrypt } from "crypto";
  import { promisify } from "util";
+import { NotFoundError } from "rxjs";
 
 
  const scrypt = promisify(_scrypt);
@@ -38,7 +39,22 @@
         return user; 
     }
 
-    signin() {
+    async signin(email: string, password: string) {
 
+        const [user] = await this.usersService.find(email);
+
+        if (!user){
+            throw new NotFoundException('user not found')
+        }
+
+        const [salt, storedHash] = user.password.split('.');
+
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+        if (storedHash !== hash.toString('hex')){
+            throw new BadRequestException('bad password')
+        }
+
+        return user; 
     }
  }
