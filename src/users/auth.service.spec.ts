09e6ab61fed8ca0cc8f275582a140b345516ae61
markpakingan <116990353+markpakingan@users.noreply.github.com>
2024-03-01 12:36:48 -0800

    testing

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,12 +2,15 @@ import { Test } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
 import { UsersService } from "./users.service";
 import { User } from "./user.entity";
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+
 
 
 
 describe('AuthAService', () => {
 
     let service: AuthService;
+    let fakeUsersService: Partial <UsersService>
 
     beforeEach(async ()=> {
     
@@ -36,5 +39,29 @@ describe('AuthAService', () => {
 
         expect(service).toBeDefined();
     } )
+
+
+    it('creates a new user with a salt and hashed password', async()=> {
+        const user = await service.signup('testuser@yahoo.com', 'asdf')
+
+        expect(user.password).not.toEqual('asdf');
+        const [ salt, hash] = user.password.split('.');
+        expect(salt).toBeDefined();
+        expect(hash).toBeDefined();
+
+    })
+
+    it('throws an error if user signs up with email that is in use', async () => {
+        fakeUsersService.find = () =>
+     
+          Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+        await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+          BadRequestException,
+        );
+      });
+    
 });
 
+
+
+//testing
