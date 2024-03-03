1217fb9524c592946accce3ce5eff49c78d587af
markpakingan <116990353+markpakingan@users.noreply.github.com>
2024-03-03 08:34:49 -0800

    fixed error in auth.service.test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -51,15 +51,21 @@ describe('AuthAService', () => {
 
     })
 
-    it('throws an error if user signs up with email that is in use', async () => {
-        fakeUsersService.find = () =>
+    // it('throws an error if user signs up with email that is in use', async () => {
+    //     fakeUsersService.find = () =>
      
-          Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
-        await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
-          BadRequestException,
-        );
-      });
+    //       Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+    //     await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+    //       BadRequestException,
+    //     );
+    //   });
+
     
+    it('throws if signin is called with an unused email', async () => {
+        await expect(
+          service.signin('asdflkj@asdlfkj.com', 'passdflkj'),
+        ).rejects.toThrow(NotFoundException);
+      });
 });
 
 
