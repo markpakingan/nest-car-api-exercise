73c57b5c72ac1257b76bf60e6e32783ec893191b
markpakingan <116990353+markpakingan@users.noreply.github.com>
2024-03-07 19:27:29 -0800

    integration test setup 1

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 // import { CookieSession } from 'cookie-session';
+import { setupApp } from './setup-app';
 
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys: ['sdfsdfdsfsd'],
-  }))
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true
-    })
-  )
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/setup-app.ts
@@ -0,0 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
+const cookieSession = require('cookie-session');
+
+
+export const setupApp = (app: any) => {
+    app.use(cookieSession({
+        keys: ['sdfsdfdsfsd'],
+      }))
+      app.useGlobalPipes(
+        new ValidationPipe({
+          whitelist: true
+        })
+      )
+     
+}
