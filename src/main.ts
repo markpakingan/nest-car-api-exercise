9a0a975f551f1be489ec5607e73c98a86f2bbd1f
markpakingan <116990353+markpakingan@users.noreply.github.com>
2024-03-09 10:22:48 -0800

    added db_name configuration

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.use();
+//   await app.listen(3000);
+// }
+// bootstrap();
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use();
+  (app as any).set('etag', false);
+  app.use((req, res, next) => {
+    res.removeHeader('x-powered-by');
+    res.removeHeader('date');
+    next();
+  });
   await app.listen(3000);
 }
 bootstrap();
